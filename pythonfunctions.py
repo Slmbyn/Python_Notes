4a8e0c88e9e6648786af2b44ad06e1d4fdc9d4d4
SelamB <selambeyene2@gmail.com>
2023-12-04 14:15:26 -0800

    finishes python functions lesson

--- pythonfunctions.py
+++ pythonfunctions.py
@@ -70,3 +70,35 @@ def dev_skills(dev_name, **kwargs):
 print(dev_skills('Jackie', HTML=5, CSS=3, JavaScript=4, Python=2))
 
 # Combining Required Positional, Optional Positional (*args) & Keyword (**kwargs) Arguments:
+'''You can use all three types of parameters in the same function, but 
+    they have to be defined in the following order:     '''
+
+def arg_demo(pos1, pos2, *args, **kwargs):
+  print(f'Positional params: {pos1}, {pos2}')
+  print('*args:')
+  for arg in args:
+    print(' ', arg)
+  print('**kwargs:')
+  for keyword, value in kwargs.items():
+    print(f'  {keyword}: {value}')
+
+arg_demo('A', 'B', 1, 2, 3, color='purple', shape='circle')
+
+'''
+Output:
+
+Positional params: A, B
+*args:
+  1
+  2
+  3
+**kwargs:
+  color: purple
+  shape: circle
+'''
+
+'''
+EXTRA *args & **kwargs INFO:
+https://yasoob.me/2013/08/04/args-and-kwargs-in-python-explained/
+
+'''
